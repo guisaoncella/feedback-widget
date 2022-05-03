104c643f4b0a6e464050b78a7a493f18c1019153
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-05-03 15:59:46 -0300

    add submit handle and button opacity

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
@@ -11,7 +11,18 @@ interface FeedbackContentStepProps {
 
 export function FeedbackContentStep({feedbackType, onRestartFeedback}: FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+    
     const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState("");
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment,
+        });
+        console.log(screenshot);
+    }
 
     return (
         <>
@@ -31,7 +42,7 @@ export function FeedbackContentStep({feedbackType, onRestartFeedback}: FeedbackC
                 <CloseButton/>
             </header>
 
-            <form className="my-4 w-full">
+            <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
                 <textarea
                     className="min-w-[304px] min-h-[112px] w-full text-sm placeholder-zinc-400 text-zinc-100
                         border-zinc-600 bg-transparent rounded-md resize-none
@@ -39,6 +50,7 @@ export function FeedbackContentStep({feedbackType, onRestartFeedback}: FeedbackC
                         scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
 
                     placeholder="Conte-nos com detalhes o que está acontecendo"
+                    onChange={(e) => setComment(e.target.value)}
                 />
 
                 <footer className="flex gap-2 mt-2">
@@ -49,10 +61,12 @@ export function FeedbackContentStep({feedbackType, onRestartFeedback}: FeedbackC
 
                     <button
                         type="submit"
+                        disabled={!comment}
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm 
                             hover:bg-brand-300 hover:ring-2 hover:ring-brand-500 hover:ring-offset-2 hover:ring-offset-zinc-900
                             focus:outline-none focus:ring-2 focus:ring-brand-500 
-                            focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors"
+                            focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors
+                            disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
 
                     Enviar Feedback        
