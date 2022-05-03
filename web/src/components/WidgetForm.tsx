f0733c81e9e0142a7cb3de039ea43dcb0929b867
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-05-03 13:05:55 -0300

    add header and footer to widget form

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -3,7 +3,16 @@ export function WidgetForm(){
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg 
                         w-[calc(100vw-2rem)] md:w-auto">
 
-            Formulário do Widget
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+            </header>
+
+            <p>Formulário do Widget</p>
+
+            <footer className="text-xs text-neutral-400">
+                <a className="underline underline-offset-2" target="_blank" rel="noreferrer" href="https://github.com/guisaoncella">guisaoncella</a> ✨ 
+                <a className="underline underline-offset-2" target="_blank" rel="noreferrer" href="https://rocketseat.com.br">Rocketseat NLW</a>
+            </footer>
 
         </div>
     );
