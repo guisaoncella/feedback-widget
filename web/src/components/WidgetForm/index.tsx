5f297c27b6811ab3f19e88078883951e11b3ad5c
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-05-03 14:22:41 -0300

    refactor header to feedback type step

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thinkImageUrl from "../../assets/think.svg";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
     BUG:{
@@ -38,15 +39,10 @@ export function WidgetForm(){
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg 
                         w-[calc(100vw-2rem)] md:w-auto">
 
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-                <CloseButton/>
-            </header>
-
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />    
             ) : (
-                <p>Selecionado!</p>
+                <FeedbackContentStep/>
             )}
 
             <footer className="text-xs text-neutral-400">
