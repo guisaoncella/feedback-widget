5bf95c9e67368cd17b842e2c4c22a9c99d3ef999
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-05-09 16:22:44 -0300

    add label invalid email

--- web/src/components/Page/TestForm/TestFormInput.tsx
+++ web/src/components/Page/TestForm/TestFormInput.tsx
@@ -1,11 +1,14 @@
 export function TestFormInput(){
+    const isEmailValid = true;
     return(
         <>
-            <label>Email teste:
+            <label className="text-right">Email teste:
                 <input id="inputEmail" type="email" className="sm:ml-6 ml-2 text-sm h-fit
                         border-zinc-400 bg-transparent rounded-md
                         focus:border-brand-500 focus:outline-none focus:ring-brand-500 focus:ring-1"></input>
+                {!isEmailValid ? <p className="text-xs text-red-500">Email inválido</p> : <p className="text-xs opacity-0">fake</p>}
             </label>
+           
         </>     
     );
 }
